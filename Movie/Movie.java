351fe0732be3cbb6a9e28dc42706b132449cf1f1
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-10 18:15:06 +0100

    Movie(java): add class with variables

---
+++ Movie/Movie.java
@@ -0,0 +1,17 @@
+package Movie;
+
+public class Movie {
+
+  public static void main(String[] args) {
+    String movieTitle = "Indiana Jones and the Temple of Doom";
+    boolean hasSeen = true;
+    int releaseYear = 1984;
+    double imdbRating = 7.5;
+
+    System.out.println("Movie: " + movieTitle);
+    System.out.println("Have I seen it? " + hasSeen);
+    System.out.println("Year: " + releaseYear);
+    System.out.println("IMDB Note: " + imdbRating);
+
+  }
+}
