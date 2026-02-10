6a59db54e6033475ea26844b6de33d4ea0bf9ca2
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-10 17:00:29 +0100

    HelloWorld/Senpai(java): add basic class

---
+++ JavaIntro/HelloWorld.java
@@ -0,0 +1,9 @@
+package JavaIntro;
+
+class HelloWorld {
+
+  public static void main(String[] args) {
+    // afficher Hello World! dans le terminal
+    System.out.println("Hello World!");
+  }
+}
